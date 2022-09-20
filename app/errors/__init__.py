faa9436678c7ce018d8ab999368b48fdac61e05d
lingul <linneagullmak96@gmail.com>
2022-09-20 15:02:48 +0200

    first commit

--- app/errors/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Create errors Blurprint
-"""
-from flask import Blueprint
-
-bp = Blueprint('errors', __name__)
-
-from app.errors import handlers    #pylint: disable=wrong-import-position, cyclic-import
--- scripts/deploy-app/resources/setup.sql
+++
@@ -1,4 +0,0 @@
-create database <db-name> character set utf8 collate utf8_bin;
-create user '<user>'@'localhost' identified by '<password>';
-grant all privileges on <db-name>.* to '<user>'@'localhost';
-flush privileges;
